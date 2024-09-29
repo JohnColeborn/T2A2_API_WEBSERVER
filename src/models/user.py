a0f1b9e1d784ffd0434f0435a873094c0836a0f4
JohnColeborn <jwcoleborn@gmail.com>
2024-09-29 11:19:06 +0800

    updated readme

--- src/models/user.py
+++ src/models/user.py
@@ -5,7 +5,7 @@ from marshmallow.validate import Regexp
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False)
+    name = db.Column(db.String, nullable = True)
     email = db.Column(db.String, nullable = False, unique = True)
     password = db.Column(db.String, nullable = False)
      
